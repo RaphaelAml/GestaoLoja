1c272dea64e1c29394ffc16f8d5f8c31a717e6d0
RaphaelAml <raphaelaml1@outlook.com>
2023-07-12 20:05:41 -0300

    Criação de Classe

--- src/dominio/Usuario.java
+++ src/dominio/Usuario.java
@@ -1,2 +1,111 @@
-package dominio;public class Usuario {
+package dominio;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Usuario {
+
+    private int id;
+    private String nome;
+    private String senha;
+    private String usuario;
+    private Perfil perfil;
+    private boolean estado;
+    private LocalDateTime dataHoraCriacao;
+    private LocalDateTime ultimoLogin;
+
+    public Usuario() {
+
+
+    }
+
+    public Usuario(int id, String nome, String senha, String usuario, Perfil perfil, boolean estado,
+                   LocalDateTime dataHoraCriacao, LocalDateTime ultimoLogin) {
+        this.id = id;
+        this.nome = nome;
+        this.senha = senha;
+        this.usuario = usuario;
+        this.perfil = perfil;
+        this.estado = estado;
+        this.dataHoraCriacao = dataHoraCriacao;
+        this.ultimoLogin = ultimoLogin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public LocalDateTime getDataHoraCriacao() {
+        return dataHoraCriacao;
+    }
+
+    public void setDataHoraCriacao(LocalDateTime dataHoraCriacao) {
+        this.dataHoraCriacao = dataHoraCriacao;
+    }
+
+    public LocalDateTime getUltimoLogin() {
+        return ultimoLogin;
+    }
+
+    public void setUltimoLogin(LocalDateTime ultimoLogin) {
+        this.ultimoLogin = ultimoLogin;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id == usuario.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
