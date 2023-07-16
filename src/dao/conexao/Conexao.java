45672679ea088dad3e952fc3f3bc5e206fa0d598
RaphaelAml <raphaelaml1@outlook.com>
2023-07-16 09:18:12 -0300

    Criada classe Conexao

---
+++ src/dao/conexao/Conexao.java
@@ -0,0 +1,9 @@
+package dao.conexao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface Conexao {
+
+    public Connection obterConexao () throws SQLException;
+}
---
+++ src/dao/conexao/ConexaoMysql.java
@@ -0,0 +1,23 @@
+package dao.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMysql implements Conexao {
+
+
+    private final String USUARIO = "root";
+    private final String SENHA = "2636";
+    private final String URL = "jdbc:mysql://localhost/gestao_vendas?useTimezone=true&serverTimezone=UTC";
+    private Connection conectar;
+
+    @Override
+    public Connection obterConexao() throws SQLException {
+
+        if(conectar == null){
+            conectar = DriverManager.getConnection(URL,USUARIO,SENHA);
+        }
+        return conectar;
+    }
+}
