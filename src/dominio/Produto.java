9306edc8b05bcf10fe68c7fafb28ea966b34b278
RaphaelAml <raphaelaml1@outlook.com>
2023-07-16 08:31:15 -0300

    Criada classe Produto

---
+++ src/dominio/Produto.java
@@ -0,0 +1,96 @@
+package dominio;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Produto {
+
+    private int id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+    private Integer quantidade;
+    private Categoria categoria;
+    private Usuario usuario;
+    private LocalDateTime dataHoraCriacao;
+
+
+    public Produto() {
+    }
+
+    public Produto(int id, String nome, String descricao, BigDecimal preco, Integer
+            quantidade, Categoria categoria, Usuario usuario, LocalDateTime dataHoraCriacao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+        this.usuario = usuario;
+        this.dataHoraCriacao = dataHoraCriacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDateTime getDataHoraCriacao() {
+        return dataHoraCriacao;
+    }
+
+    public void setDataHoraCriacao(LocalDateTime dataHoraCriacao) {
+        this.dataHoraCriacao = dataHoraCriacao;
+    }
+}
